b1359a515f0e5d38e06f84dfa328157c9f55871a
Jonathan Araya <j.arayarobles@gmail.com>
2022-07-11 04:29:48 -0400

    primer commit

---
+++ useForm.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+
+export const useForm = ( initialForm = {} ) => {
+
+    const [formState, setFormState] = useState(initialForm);
+
+    const handleInputChange = ({target}) => {
+        setFormState({
+            ...formState,
+            [target.name]: target.value
+        })
+    };
+
+    const onResetForm = () => {
+        setFormState(initialForm);
+    };
+
+    return {
+        ...formState,
+        formState, 
+        setFormState,
+        onResetForm,
+        handleInputChange
+    };
+    
+}
---
+++ useTodos.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useReducer } from 'react';
+import { todoReducer } from '../08-useReducer/todoReducer';
+
+const init = () => {
+    const todos = JSON.parse(localStorage.getItem('todos') || '[]');
+    return todos || [];
+};
+
+export const useTodos = () => {
+
+
+    const [todos, dispatchTodo] = useReducer(todoReducer,[], init);
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+
+    }, [todos]);
+
+    const handleAddTodo = (todo) => {
+        dispatchTodo({
+            type: 'ADD_TODO',
+            payload: todo,
+        });
+    };
+
+    const handleDeleteTodo = (id) => {
+        dispatchTodo({
+            type: 'REMOVE_TODO',
+            payload: id,
+        });
+    };
+
+    const handleToggleTodo = (id) => {
+        dispatchTodo({
+            type: 'TOGGLE_TODO',
+            payload: id,
+        });
+    };
+
+    const todosCount = todos.length;
+    const pendingTodosCount = todos.filter(todo => !todo.done).length;
+
+    return {
+        todos,
+        handleAddTodo,
+        handleDeleteTodo,
+        handleToggleTodo,
+        todosCount,
+        pendingTodosCount,
+    };
+};
