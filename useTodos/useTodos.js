ef20b3c59e663576fe5cb189d475601701832938
Jonathan Araya <j.arayarobles@gmail.com>
2022-07-11 04:45:20 -0400

    useTodos agregado

--- useTodos/useTodos.js
+++ useTodos/useTodos.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useReducer } from 'react';
-import { todoReducer } from '../08-useReducer/todoReducer';
+import { todoReducer } from './todoReducer';
 
 const init = () => {
     const todos = JSON.parse(localStorage.getItem('todos') || '[]');
